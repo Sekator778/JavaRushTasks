ee7942550abf19c6d9549bfa3ad9292a19c7a262
Sekator <sekator778@gmail.com>
2020-11-23 17:23:51 +0200

    Встречаемость символов int -> char -> int

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -1,11 +1,34 @@
 package com.javarush.task.task18.task1821;
 
-/* 
+/*
 Встречаемость символов
 */
 
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * FileReader.ready() читает инт чара - это и есть номер индекса а значение инкрементим каждый раз
+ * вывод на экран кастуем (char) i == символ
+ */
 public class Solution {
     public static void main(String[] args) {
-
+        if (args.length == 0) {
+            System.out.println("#############  no file name in args  ###############");
+            return;
+        }
+        int[] aSCII = new int[128];
+        try (FileReader reader = new FileReader(args[0])) {
+            while (reader.ready()) {
+                aSCII[reader.read()]++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < aSCII.length; i++) {
+            if (aSCII[i] != 0) {
+                System.out.println((char) i + " " + aSCII[i]);
+            }
+        }
     }
 }
--- Приклади/helloworld.java
+++ Приклади/helloworld.java
@@ -1,4 +1,4 @@
-public class Helloworld {
+class Helloworld {
 public static void main (String[] args) {
 	// Prints "Hello world " to the terminal windows
 System.out.println("Hello world !");
