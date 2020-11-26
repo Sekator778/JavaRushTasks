46bebc1f74c1e582b255cbda04f875820624423f
Sekator778 <sekator778@gmail.com>
2020-11-26 22:44:03 +0200

    пример заполнения log4j.properties

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3409/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task34.task3409;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/* 
+Настраиваем логгер
+*/
+
+public class Solution {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Solution.class);
+    public static void main(String[] args) throws IOException {
+        String logProperties = "4.JavaCollections/src/" + Solution.class.getPackage().getName().replaceAll("[.]", "/") + "/log4j.properties";
+        System.out.println(logProperties);
+        Path path = Paths.get(logProperties).toAbsolutePath();
+        try (InputStream is = new FileInputStream(path.toFile())) {
+            Properties properties = new Properties();
+            properties.load(is);
+        }
+    }
+}
