e0c63bbac7effee32d9c31c5e478b8017ab45b52
Sekator778 <sekator778@gmail.com>
2020-11-30 21:57:13 +0200

    FileInputStream

--- 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
@@ -1,10 +1,24 @@
 package com.javarush.task.task18.task1824;
 
+import java.io.*;
+import java.util.Scanner;
+
 /* 
 Файлы и исключения
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String fileName2 = scanner.nextLine();
+            try (FileInputStream fileInputStream = new FileInputStream(fileName2)) {
+
+            } catch (FileNotFoundException e) {
+                System.out.println(fileName2);
+                scanner.close();
+                break;
+            }
+        }
     }
 }
