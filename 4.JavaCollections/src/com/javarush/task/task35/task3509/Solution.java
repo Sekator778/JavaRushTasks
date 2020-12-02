6323936bdcd7fa0ecd3ff9ee94c4929203e055fd
Sekator778 <sekator778@gmail.com>
2020-12-02 21:10:41 +0200

    generic collections

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3509/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task35.task3509;
+
+import java.util.*;
+
+/* 
+Collections & Generics
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static ArrayList newArrayList(Object... elements) {
+        //напишите тут ваш код
+        return null
+    }
+
+    public static HashSet newHashSet(Object... elements) {
+        //напишите тут ваш код
+        return null
+    }
+
+    public static HashMap newHashMap(List keys, List values) {
+        //напишите тут ваш код
+        return null;
+    }
+}
