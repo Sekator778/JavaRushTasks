5a0bcb547afe40378a4eb3f7760780e0cd2c7f0c
Sekator778 <sekator778@gmail.com>
2020-12-16 19:55:54 +0200

    Повторил паттерн Стратегия. - Получил опыт работы с библиотекой Guava. - Получил опыт работы с Apache Commons Collections. - Попробовал писать тесты, используя Junit. - Еще на шаг продвинулся к работе Java программистом.

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -1,62 +1,68 @@
 package com.javarush.task.task33.task3310;
 
-import com.javarush.task.task33.task3310.strategy.HashMapStorageStrategy;
-import com.javarush.task.task33.task3310.strategy.StorageStrategy;
-
+import com.javarush.task.task33.task3310.strategy.*;
 
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-
 public class Solution {
-    public static Set<Long> getIds(Shortener shortener, Set<String> strings) {
-        Set<Long> result = new HashSet<>();
-        for (String s : strings
-        ) {
-            result.add(shortener.getId(s));
-        }
-        return result;
-    }
+    public static void main(String[] args) {
+        long elementsNumber = 10000;
 
-    public static Set<String> getStrings(Shortener shortener, Set<Long> keys) {
-        Set<String> result = new HashSet<>();
-        for (Long s : keys
-        ) {
-            result.add(shortener.getString(s));
-        }
-        return result;
+        testStrategy(new HashMapStorageStrategy(), elementsNumber);
+
+        testStrategy(new FileStorageStrategy(), elementsNumber);
+
+        testStrategy(new OurHashBiMapStorageStrategy(), elementsNumber);
+
+        testStrategy(new OurHashMapStorageStrategy(), elementsNumber);
     }
 
     public static void testStrategy(StorageStrategy strategy, long elementsNumber) {
         Helper.printMessage(strategy.getClass().getSimpleName() + ":");
-        Set<String> strings = new HashSet<>();
-        for (int i = 0; i < elementsNumber; i++) {
-            String nextRandomString = Helper.generateRandomString();
-            strings.add(nextRandomString);
-//            Helper.printMessage(nextRandomString);
-        }
-        Shortener shortener = new Shortener(strategy);
-        Date startTime= new Date();
-        Set<Long> keys = getIds(shortener, strings);
-        Date endTime = new Date();
-        long time = endTime.getTime() - startTime.getTime();
-        Helper.printMessage("Время получения идентификаторов для " + elementsNumber + " строк: " + time);
-        startTime= new Date();
-        Set<String> stringSet = getStrings(shortener, keys);
-        endTime = new Date();
-        long time2 = endTime.getTime() - startTime.getTime();
-        Helper.printMessage("Время получения строк для " + elementsNumber + " идентификаторов: " + time2);
-        if (strings.equals(stringSet)) {
-            Helper.printMessage("Тест пройден.");
-        } else {
-            Helper.printMessage("Тест не пройден.");
+
+        Set<String> origStrings = new HashSet<>();
+
+        for (int i = 0; i < elementsNumber; ++i) {
+            origStrings.add(Helper.generateRandomString());
         }
 
+        Shortener shortener = new Shortener(strategy);
+
+        Date startTimestamp = new Date();
+        Set<Long> keys = getIds(shortener, origStrings);
+        Date endTimestamp = new Date();
+        long time = endTimestamp.getTime() - startTimestamp.getTime();
+        Helper.printMessage("Время получения идентификаторов для " + elementsNumber + " строк: " + time);
+
+        startTimestamp = new Date();
+        Set<String> strings = getStrings(shortener, keys);
+        endTimestamp = new Date();
+        time = endTimestamp.getTime() - startTimestamp.getTime();
+        Helper.printMessage("Время получения строк для " + elementsNumber + " идентификаторов: " + time);
+
+        if (origStrings.equals(strings))
+            Helper.printMessage("Тест пройден.");
+        else
+            Helper.printMessage("Тест не пройден.");
+
+        Helper.printMessage("");
     }
 
-    public static void main(String[] args) {
-        long elementsNumber = 10000;
-        testStrategy(new HashMapStorageStrategy(), elementsNumber);
+    public static Set<Long> getIds(Shortener shortener, Set<String> strings) {
+        Set<Long> keys = new HashSet<>();
+        for (String s : strings) {
+            keys.add(shortener.getId(s));
+        }
+        return keys;
+    }
+
+    public static Set<String> getStrings(Shortener shortener, Set<Long> keys) {
+        Set<String> strings = new HashSet<>();
+        for (Long k : keys) {
+            strings.add(shortener.getString(k));
+        }
+        return strings;
     }
 }
